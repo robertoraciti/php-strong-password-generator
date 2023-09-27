248758cf4202ebd09ca6f82070d3aabd512b96da
robertoraciti <rob.raciti97@gmail.com>
2023-09-27 17:23:29 +0200

    Milestone 1

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale
+ (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. -->
+
+
+<?php
+function generate_psw()
+{
+    $possible_char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+
+    $password = [];
+
+    $numb_of_char = (int) $_GET['char'];
+
+    $comb_char = strlen($possible_char) - 1;
+
+    for ($i = 0; $i < $numb_of_char; $i++) {
+        $random_psw = rand(0, $comb_char);
+        $password[] = $possible_char[$random_psw];
+    }
+
+    return implode($password);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +46,32 @@
     <link rel="stylesheet" href="./css/style.css">
 
     <!-- BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
-    
+    <div class="mt-5 text-center">
+        <div class="card w-50 m-auto">
+            <div class="card-header">Lunghezza Password da generare</div>
+            <div class="card-body">
+                <form method="get">
+                    <input type="number" name="char" id="char" class="form-control" min="1" max="20">
+                    <button class="btn btn-primary mt-3">Genera</button>
+                </form>
+                <h1>
+                    La tua password è:
+                    <?php echo generate_psw() ?>
+                </h1>
+
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
